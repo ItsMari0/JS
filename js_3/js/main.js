ebcae5cb976254ea3c5f5a584818320dd0ff7596
ItsMari0 <evgen.mazurkevich@gmail.com>
2022-01-18 21:55:28 +0300

    need some fix

--- js_3/js/main.js
+++ js_3/js/main.js
@@ -124,39 +124,32 @@ console.log(
  * car.used – строка для описания состояния авто, допускаются значения 'used' и 'new'
  */
 var yearNow = new Date().getFullYear(); // получить текущий год как число
-var car = {
-  name: "Audi",
-  model: "A4",
-  year: "2021",
-  /*used: "used",*/
-  engine: "Gasoline",
+let car = {
+  engine: 2000,
+  model: "Lacetti",
+  name: "Chevrolet",
+  year: 2010,
   info: carInfo,
   get used() {
-    return yearNow - this.year ? "used" : "new";
+    return yearNow != this.year ? "used" : "new";
   },
   set used(value) {
-    if (value === "new" && this.year < yearNow) {
-      this.year = yearNow;
-    }
+    if (value === "new" && this.year < yearNow) this.year = yearNow;
   },
 };
-var car2 = {
-  name: "BMW",
-  model: "M5",
-  year: "2022",
-  /* used: "new",*/
-  engine: "Gasoline",
+let car1 = {
+  engine: "2021",
+  model: "BMW",
+  name: "M5",
+  year: 2021,
   info: carInfo,
+  get used() {
+    return yearNow != this.year ? "used" : "new";
+  },
+  set used(value) {
+    if (value === "new" && this.year < yearNow) this.year = yearNow;
+  },
 };
-var car3 = {
-  name: "Tesla",
-  model: "Model X",
-  year: "2020",
-  /* used: "used",*/
-  engine: "electro",
-  info: carInfo,
-};
-
 console.log(
   "/************************************************************************************ */"
 );
@@ -170,23 +163,22 @@ console.log(
  * пробелы, запятые, символы cc и текст – имеют значение и проверяются при тестировании кода
  */
 function carInfo() {
-  var str =
+  return (
     this.name +
-    ", " +
+    " " +
     this.model +
     ", " +
+    this.engine +
+    "cc, year " +
     this.year +
-    ", " +
-    this.used +
-    ", " +
-    this.engine;
-  return str;
+    " " +
+    this.used
+  );
 }
 /*console.log(car.info());*/
 console.log(
   "/************************************************************************************ */"
 );
-console.log(typeof car.engine);
 /*
  * #6
  *
@@ -204,9 +196,9 @@ car.used = "new";
 console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2019, new -- год изменен
 car.used = "used";
 console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2019, new -- изменения не выполняются
-console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new
+console.log(car1.info()); // Infinite FX50 AWD, 5000cc, year 2019, new
 car.used = "used";
-console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new -- изменения не выполняются
+console.log(car1.info()); // Infinite FX50 AWD, 5000cc, year 2019, new -- изменения не выполняются
 
 /*
  * #7
@@ -214,37 +206,44 @@ console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new -- изм
  * произвольный числовой массив и возвращает максимальное число из переданного ей массива.
  * В реализации функции должен быть применен метод Math.max() и apply().
  */
-
-// let list = [12, 23, 100, 34, 56, 9, 233];
-
-// console.log(myMax(list)); // 233
-
+console.log(
+  "/************************************************************************************ */"
+);
+let list = [12, 23, 100, 34, 56, 9, 233];
+var myMax = function (a) {
+  return Math.max.apply(Math, a);
+};
+console.log(myMax(list)); // 233
+console.log(
+  "/************************************************************************************ */"
+);
 /*
  * #8
  *
  * Создайте функцию myMul(a, b), которая будет умножать числа а и b, возвращая результат.
  */
-
+function myMul(a, b) {
+  return a * b;
+}
 /*
  * создайте функции myDouble(n), которая принимает один параметр и  удваивает его.
  * Использовать умножение или другие математические операции внутри функции – запрещено, только bind() и myMul().
  * Функция возвращает результат вычисления.
  */
+var myDouble = myMul.bind(null, 2);
 
-// console.log(myDouble(3)); // = myMul(2, 3) = 6
-
-// console.log(myDouble(4)); // = myMul(2, 4) = 8
-
-// console.log(myDouble(5)); // = myMul(2, 5) = 10
-
+console.log(myDouble(3)); // = myMul(2, 3) = 6
+console.log(myDouble(4)); // = myMul(2, 4) = 8
+console.log(myDouble(5)); // = myMul(2, 5) = 10
 // аналогичным образом создайте функцию myTriple(n), которая утраивает принимающий параметр, возвращая результат.
+var myTriple = myMul.bind(null, 3);
+console.log(myTriple(3)); // = myMul(3, 3) = 9
+console.log(myTriple(4)); // = myMul(3, 4) = 12
+console.log(myTriple(5)); // = myMul(3, 5) = 15
 
-// console.log(myTriple(3)); // = myMul(3, 3) = 9
-
-// console.log(myTriple(4)); // = myMul(3, 4) = 12
-
-// console.log(myTriple(5)); // = myMul(3, 5) = 15
-
+console.log(
+  "/************************************************************************************ */"
+);
 /*
  * #9
  *
@@ -255,10 +254,29 @@ console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new -- изм
  * Любые условные операторы – запрещены и объекты.
  */
 
-// let notUniqNums = [1, 1, 2, 3, 4, 5, 6, 7];
+let notUniqNums = [1, 1, 2, 3, 4, 5, 6, 7];
+let notUniqStrings = [
+  "Bob",
+  "Kate",
+  "Jhon",
+  "Tom",
+  "Jhon",
+  "Kate",
+  "Tom",
+  "Bob",
+  "Jhon",
+  "Tom",
+];
 
-// let notUniqStrings = ['Bob', 'Kate', 'Jhon', 'Tom', 'Jhon', 'Kate', 'Tom', 'Bob', 'Jhon', 'Tom'];
-
-// console.log(myUniq(notUniqNums));
-
-// console.log(myUniq(notUniqStrings));
+function myUniq(arr) {
+  let newArr = new Set();
+  arr.forEach((element) => {
+    newArr.add(element);
+  });
+  return newArr;
+}
+console.log(myUniq(notUniqNums));
+console.log(myUniq(notUniqStrings));
+console.log(
+  "/************************************************************************************ */"
+);
